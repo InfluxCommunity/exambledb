3647c9608a0d1f1c5aa1909fffbb8948d2e24b0c
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-10-17 10:13:05 +0200

    Add primary key to example

--- write.py
+++ write.py
@@ -3,15 +3,18 @@ import requests
 data = [{
     "table":"mytable",
     "col1": "two",
-    "col2": 2
+    "col2": 2,
+    "primary_key": ["col1"]
 }, {
     "table":"mytable",
     "col1": "three",
-    "col2": 3
+    "col2": 3,
+    "primary_key": ["col1"]
 },{
     "table":"mytable",
     "col1": "four",
-    "col2": 4
+    "col2": 4,
+    "primary_key": ["col1"]
 } ]
 
 response = requests.post("http://localhost:5001/write", json=data)
